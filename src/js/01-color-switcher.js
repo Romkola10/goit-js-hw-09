cf1bab2da1e3a145902d02b23d57835f131d46a7
Romkola10 <57733600+Romkola10@users.noreply.github.com>
2022-06-27 01:29:39 +0300

    finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const bodyRef = document.querySelector('body');
+const startButtonRef = document.querySelector('button[data-start]');
+const stopButtonRef = document.querySelector('button[data-stop]');
 
+const timer = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      ChangeColorRef();
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+
+startButtonRef.addEventListener('click', () => {
+  timer.start();
+});
+
+stopButtonRef.addEventListener('click', () => {
+  timer.stop();
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const ChangeColorRef = () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('[name=delay]');
+const stepInput = document.querySelector('[name=step]');
+const amountInput = document.querySelector('[name=amount]');
+let position = 0;
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  setTimeout(() => {
+    for (let i = 0; i < amountInput.value; i += 1) {
+      const step = +delayInput.value + +stepInput.value * i;
+      position = i + 1;
+      createPromise(position, step)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay} ms`);
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay} ms`);
+          Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
+        });
+    }
+  }, delayInput.value);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
